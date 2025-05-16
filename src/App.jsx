4c4689c89afba7ed45b5db0a22c073c97ec10fb3
miko292002 <arnad.johnmiecho@wealthbank.com.ph>
2025-05-16 14:50:00 +0800

    Add Tailwind CSS integration and restructure App component for improved layout

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,43 @@
 import React from 'react'
 import './App.css'
 
+
 function App() {
   return (
     
-      <nav className="Navbar">
-        <ul className='navlinks'>
-          <div className='name'>
-          <p>John Miecho Arnad</p>
-          </div>
-          <li><a href=''>Projects</a></li>
-          <li><a href=''>Resume</a></li>
-          <li><a href=''>Certificates</a></li>
-        </ul>
-      </nav>
+    <div className="min-h-screen bg-[#0d0d0d] text-white flex flex-col">
+    <div className="flex justify-between items-center px-4 md:px-8 py-4 text-sm">
+      <a href="#home" className='space-x-4 md:text-base'>John Miecho Catalan Arnad</a>
+      <div className="space-x-4 md:space-x-6 text-xs md:text-sm">
+        <a href="#projects" className="hover:underline">Projects</a>
+        <a href="#resume" className="hover:underline">Resume</a>
+        <a href="#certificates" className="hover:underline">Certificates</a>
+      </div>
+    </div>
+
+    <div className="flex flex-col-reverse md:flex-row flex-1 items-center justify-center px-4 md:px-8 space-y-8  md:space-y-0">
+      <div className="max-w-xl text-center md:text-left">
+        <h1 className="text-3xl md:text-4xl font-semibold mb-4">Hi, I’m Miko<span className="text-white">.</span></h1>
+        <p className=" text-base md:text-lg text-gray-300 mb-6">
+          A Hard working Fresh Graduate Software Developer and QA from Cebu City, Philippines. I specialize at React JS, Tailwind and Nest JS.
+        </p>
+        <a
+          href="#projects"
+          className="bg-indigo-500 hover:bg-indigo-600 text-white px-6 py-3 rounded-full transition inline-block"
+        >
+          My Projects
+        </a>
+      </div>
+
+      <div className="w-full md:w-auto flex justify-center">
+        <img
+          src="/1000001470.png"
+          alt="Miko"
+          className="rounded-b-full h-60 md:h-80 object-cover"
+        />
+      </div>
+    </div>
+  </div>
   )
 }
 
