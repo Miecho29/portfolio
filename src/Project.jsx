10ec16264b2edb5fd76ea1669cf378e59a872eef
miko292002 <arnad.johnmiecho@wealthbank.com.ph>
2025-05-19 16:26:47 +0800

    Refactor Homepage layout and update Project component styling; add new image for Project section

--- src/Project.jsx
+++ src/Project.jsx
@@ -16,19 +16,13 @@ function Project(){
     </div>
 
       
-<a href="#" class="flex flex-col items-center bg-white border border-gray-200 rounded-lg shadow-sm md:flex-row md:max-w-xl hover:bg-gray-100 dark:border-gray-700 dark:bg-gray-800 dark:hover:bg-gray-700">
-    {/* <img class="object-cover w-full rounded-t-lg h-96 md:h-auto md:w-48 md:rounded-none md:rounded-s-lg" src="/docs/images/blog/image-4.jpg" alt=""> */}
+<a href="#" class="flex flex-col items-center bg-[#181A1C] border border-gray-200 rounded-lg shadow-sm md:flex-row md:max-w-xl dark:border-gray-700 dark:bg-gray-800 dark:hover:bg-gray-700">
     <div class="flex flex-col justify-between p-4 leading-normal">
-        <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">Data Engineer Intern At Apteum Mar 2024 - July 2025</h5>
-        <p class="mb-3 font-normal text-gray-700 dark:text-gray-400">Focused on data maintenance and quality assurance, create a documentation of all issues found on the tool and do maintenance,fixingbugs,implement new function to improve the tool using  PHP and Laravel.</p>
+        <h5 class="mb-2 text-2xl font-bold tracking-tight text-white ">Data Engineer Intern At Apteum Mar 2024 - July 2025</h5>
+        <p class="mb-3 font-normal text-white">Focused on data maintenance and quality assurance, create a documentation of all issues found on the tool and do maintenance,fixingbugs,implement new function to improve the tool using  PHP and Laravel.</p>
     </div>
+    <img class="object-cover w-full rounded-t-lg h-96 md:h-auto md:w-48 md:rounded-none md:rounded-s-lg" src="apteum.jpg" alt="Apteum"></img>
 </a>
-
-
-
-
-
-
 </div>
     )
 }
